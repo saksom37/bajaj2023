b15d7f0fa743d74fe015d8b3c90e515e7476a227
Saksham Walia <97230927+saksom37@users.noreply.github.com>
2024-03-18 07:49:33 +0000

    commited

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const port = 3000;
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.post("/bfhl", (req, res) => {
+  const info = req.body;
+  const response = {
+    is_success: true,
+    user_id: "john_doe_17091999",
+    email: "john@xyz.com",
+    roll_number: "ABCD123",
+    odd_numbers: info.filter((num) => num % 2 !== 0),
+    even_numbers: info.filter((num) => num % 2 === 0),
+    alphabets: info
+      .filter((val) => typeof val === string)
+      .map((val) => {
+        val.toUpperCase();
+      }),
+  };
+  res.send(response);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at port : ${port}`);
+});
